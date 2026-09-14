cf66955ed9679d0cd4aed864c400060091779dd9
Art3mis2063 <143340442+Art3mis2063@users.noreply.github.com>
2026-09-14 12:32:05 +0200

    Initial commit: Backend- und Frontend-Grundgerüst mit Package-/Ordnerstruktur und Docker-Compose für MongoDB

---
+++ backend/src/main/java/org/dahllab/opsservicedoc/OpsServiceDocApplication.java
@@ -0,0 +1,13 @@
+package org.dahllab.opsservicedoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsServiceDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpsServiceDocApplication.class, args);
+    }
+
+}
