92e9bd9853f30eae24a9f860bc2657b0d8ab08b6
Art3mis2063 <143340442+Art3mis2063@users.noreply.github.com>
2026-09-17 19:39:19 +0200

    --no-edit

---
+++ backend/src/test/java/org/dahllab/opsservicedoc/service/TicketServiceTest.java
@@ -0,0 +1,27 @@
+package org.dahllab.opsservicedoc.service;
+
+import org.dahllab.opsservicedoc.dto.TicketDto;
+import org.dahllab.opsservicedoc.model.SzenarioTyp;
+import org.dahllab.opsservicedoc.model.Ticket;
+import org.dahllab.opsservicedoc.model.TicketStatus;
+import org.dahllab.opsservicedoc.repository.TicketRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+// @ExtendWith
+//
+//
+@ExtendWith(MockitoExtension.class)
+class TicketServiceTest {
+}
